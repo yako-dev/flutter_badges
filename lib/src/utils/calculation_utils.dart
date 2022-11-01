67197bd5260af278f80384e441d031198d812bf2
sawel24 <sawel.gnidenko@gmail.com>
2022-11-01 19:53:35 +0200

    feat: add unit tests for calculate methods

---
+++ lib/src/utils/calculation_utils.dart
@@ -0,0 +1,58 @@
+import 'package:badges/badges.dart';
+import 'package:flutter/material.dart';
+
+class CalculationUtils {
+
+  /// When the onTap is specified the additional padding is added
+  /// Thats why we need to recalculate the position
+  static BadgePosition calculatePosition(BadgePosition? position) {
+    if (position == null) {
+      return BadgePosition(end: 0, top: 0);
+    }
+
+    double? getUpdatedPosition(double? digit) {
+      if (digit == null) {
+        return null;
+      }
+      return !digit.isNegative ? digit : 0;
+    }
+
+    return BadgePosition(
+      start: getUpdatedPosition(position.start),
+      end: getUpdatedPosition(position.end),
+      top: getUpdatedPosition(position.top),
+      bottom: getUpdatedPosition(position.bottom),
+    );
+  }
+
+  /// When the onTap is specified, we need to add some padding
+  /// to make the full badge tappable.
+  static EdgeInsets calculatePadding(BadgePosition? position) {
+    if (position == null) {
+      return EdgeInsets.only(top: 8, right: 10);
+    }
+
+    if (position.isCenter) {
+      return EdgeInsets.zero;
+    }
+
+    double getUpdatedPadding(double? digit) {
+      if (digit == null || !digit.isNegative) {
+        return 0;
+      }
+      return digit.abs();
+    }
+
+    if (position.top != null && position.start != null) {
+      return EdgeInsets.only(
+          top: getUpdatedPadding(position.top),
+          left: getUpdatedPadding(position.start));
+    }
+    return EdgeInsets.only(
+      top: getUpdatedPadding(position.top),
+      bottom: getUpdatedPadding(position.bottom),
+      left: getUpdatedPadding(position.start),
+      right: getUpdatedPadding(position.end),
+    );
+  }
+}
